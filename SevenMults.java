ed5d9193d65450bc4d392aa00b379c9b39841f4b
Patrick Taylor <ptaylor2018@users.noreply.github.com>
2016-09-16 11:39:35 -0400

    Add assignment from today

---
+++ SevenMults.java
@@ -0,0 +1,12 @@
+
+public class SevenMults
+{
+    public static void main(String args[]){
+        int[] mults= new int[10];
+        for(int i = 0; i<10; i++){
+            mults[i]= (i+1)*7;
+            System.out.println(mults[i]);
+        }
+
+    }
+}
