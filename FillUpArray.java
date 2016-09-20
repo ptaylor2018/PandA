923fc92dd6534ea99b5a6cd0d0c8828bb30fbd10
Patrick Taylor <ptaylor2018@users.noreply.github.com>
2016-09-20 12:14:57 -0400

    Add files via upload

---
+++ FillUpArray.java
@@ -0,0 +1,14 @@
+
+public class FillUpArray
+{
+    public static void main(String [] args) {
+        int [][] fillMeUp= new int [500] [600];
+        for(int i =0; i< fillMeUp.length; i++){
+            for(int j=0; j<fillMeUp[i].length; j++){
+                fillMeUp[i][j] =5;
+            }
+
+        }
+
+    }
+}
