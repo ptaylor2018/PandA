baca9c8235c716beb31537c2832db2e413a81d4a
Patrick Taylor <ptaylor2018@users.noreply.github.com>
2016-11-22 11:34:49 -0500

    Add files via upload

--- TheSelectFew.java
+++ TheSelectFew.java
@@ -1,4 +1,5 @@
-
+import java.io.*;
+import java.util.Scanner;
 /**
  * Write a description of class TheSelectFew here.
  * 
@@ -7,27 +8,58 @@
  */
 public class TheSelectFew
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    public static void main(String args[]){
+        try{
+            Scanner scan = new Scanner(new BufferedReader(new FileReader("sortMe.txt")));
+            int size = 0;
+            while(scan.hasNext()) {
+                size++;
+                scan.next();
+            }
+            ArrayList words = new ArrayList();
+            scan.close();
+            scan = new Scanner(new BufferedReader(new FileReader("sortMe.txt")));
+            int count = 0;
+            while(scan.hasNext()) {
+                words[count] = scan.next();
+                count++;
+            }
+            String[] newWords = new String[size];
 
-    /**
-     * Constructor for objects of class TheSelectFew
-     */
-    public TheSelectFew()
-    {
-        // initialise instance variables
-        x = 0;
+            for(int i = 0; i < size; i++){
+                newWords[i] = getMinValue(words);
+                words[i] = null;
+            }
+            /*for(int i = 0; i< size; i++){
+            int checks = 0;
+            for(int j = 1; j < size; j++){
+            if(words[i].compareToIgnoreCase(words[j]) > 0){
+            checks++;
+            }
+            }
+            if(checks == size){
+            newWords[i] = words[i];
+            }
+            }*/
+            for(int k = 0; k < words.length -1 ; k++){
+                System.out.println(newWords[k]);
+            }
+
+        }
+        catch(Exception e) {
+            System.out.println("WAT: " + e.getMessage());
+        }
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static String getMinValue(String[] words){
+        String minValue = words[0];
+        for(int j = 0; j < words.length; j++){
+            if (words[j].compareToIgnoreCase(minValue) < 0){
+                minValue = words[j];
+            }
+        }
+        System.out.println(minValue);
+        return minValue;
     }
 }
+
